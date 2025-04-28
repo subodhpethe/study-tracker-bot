38a4fbf5073e803446d6fc3dbbb5ebe0af7e1430
subodhpethe <95556427+subodhpethe@users.noreply.github.com>
2025-04-28 15:47:18 +0530

    Add files via upload

--- receive_replies.py
+++ receive_replies.py
@@ -4,13 +4,17 @@ from oauth2client.service_account import ServiceAccountCredentials
 import datetime
 import os
 import json
+import base64
 
 app = Flask(__name__)
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-# Load credentials from environment variable
-credentials_info = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+# Load credentials from BASE64 encoded environment variable
+credentials_b64 = os.environ['GOOGLE_CREDENTIALS_B64']
+credentials_json = base64.b64decode(credentials_b64).decode('utf-8')
+credentials_info = json.loads(credentials_json)
+
 creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
 client = gspread.authorize(creds)
 
